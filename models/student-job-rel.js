30116e245efbd3f87145c453121ec06f153e1ae8
欧贺福 <hefu.ou@meicloud.com>
2018-03-23 15:44:24 +0800

    fixed bug

--- models/student-job-rel.js
+++ models/student-job-rel.js
@@ -2,7 +2,7 @@
  * @Author: 欧贺福
  * @Date: 2018-03-23 10:52:38
  * @Last Modified by: 欧贺福
- * @Last Modified time: 2018-03-23 15:37:48
+ * @Last Modified time: 2018-03-23 15:42:52
  */
 const utils = require('../utils/common')
 
