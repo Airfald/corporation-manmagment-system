89ea8e8575d7eefe1704a8de6834bcb0a4bbd1a3
欧贺福 <hefu.ou@meicloud.com>
2018-05-23 16:42:03 +0800

    fixed bug

--- models/corporation.js
+++ models/corporation.js
@@ -2,7 +2,7 @@
  * @Author: 欧贺福
  * @Date: 2018-03-23 10:52:38
  * @Last Modified by: 欧贺福
- * @Last Modified time: 2018-03-26 20:00:24
+ * @Last Modified time: 2018-05-20 19:40:13
  */
 const Sequelize = require('sequelize')
 const utils = require('../utils/common')
@@ -15,6 +15,10 @@ const corporationModel = utils.defineModel('corporation', {
   description: {
     type: Sequelize.STRING,
     allowNull: false,
+  },
+  examine: {
+    type: Sequelize.BOOLEAN,
+    allowNull: true,
   }
 })
 
--- models/student-info.js
+++ models/student-info.js
@@ -43,71 +43,13 @@ const studentInfoModel = utils.defineModel('student_info', {
     allowNull: true,
   },
   isAdmin: {
-    type: Sequelize.BOOLEAN,
+    type: Sequelize.INTEGER,
+    allowNull: true
+  },
+  corporationId: {
+    type: Sequelize.INTEGER,
     allowNull: true
   }
 })
 
 module.exports = studentInfoModel;
-
-// const studentInfo = db.define('student_info', {
-//   id: {
-//     type: Sequelize.INTEGER,
-//     autoIncrement: true,
-//     primaryKey: true,
-//     allowNull: false
-//   },
-//   grade: {
-//     type: Sequelize.STRING,
-//     allowNull: true,
-//   },
-//   department: {
-//     type: Sequelize.STRING,
-//     allowNull: true,
-//   },
-//   class: {
-//     type: Sequelize.STRING,
-//     allowNull: true,
-//   },
-//   number: {
-//     type: Sequelize.INTEGER,
-//     allowNull: true,
-//   },
-//   name: {
-//     type: Sequelize.STRING,
-//     allowNull: true,
-//   },
-//   password: {
-//     type: Sequelize.STRING,
-//     allowNull: true,
-//   },
-//   age: {
-//     type: Sequelize.INTEGER,
-//     allowNull: true
-//   },
-//   sex: {
-//     type: Sequelize.STRING,
-//     allowNull: true,
-//   },
-//   email: {
-//     type: Sequelize.STRING,
-//     allowNull: true,
-//   },
-//   telphone: {
-//     type: Sequelize.STRING,
-//     allowNull: true,
-//   },
-//   createdAt: {
-//     field: 'created_at',
-//     type: Sequelize.DATE,
-//     allowNull: true,
-//   },
-//   updatedAt: {
-//     field: 'updated_at',
-//     type: Sequelize.DATE,
-//     allowNull: true,
-//   }
-// }, {
-//   freezeTableName: true,      // 默认为false, 会自动改变表名称从而报错不存在改表
-//   timestamps: false           // 默认为true自动生成createdAt, updatedAt字段
-// });
